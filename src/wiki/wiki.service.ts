aee6f1019be9556819a3f2412b0a80982a7b5c46
VZGLY <chaineux.gavin@gmail.com>
2023-01-06 14:31:03 +0100

    up

--- src/wiki/wiki.service.ts
+++ src/wiki/wiki.service.ts
@@ -9,7 +9,7 @@ export class WikiService {
     constructor(private readonly _httpService : HttpService){}
 
     GetMonthly() : Observable<any>{
-        return this._httpService.get<any>('https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/de.wikipedia/all-access/all-agents/Donald_Trump/monthly/2016010100/2016123100'
+        return this._httpService.get<any>('https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/fr.wikipedia/all-access/all-agents/Donald_Trump/monthly/2016010100/2016123100'
         ).pipe(map(res => {
             return res.data
         }))
@@ -19,13 +19,57 @@ export class WikiService {
 
         let monthly : MonthlyData[] = []
 
-        const value = await firstValueFrom(this._httpService.get<any>('https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/de.wikipedia/all-access/all-agents/' + page +'/monthly/' + year + '010100/' + year + '123100'
+        const value = await firstValueFrom(this._httpService.get<any>('https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/fr.wikipedia/all-access/all-agents/' + page +'/monthly/' + year + '010100/' + year + '123100'
         ))
-        let cpt = 0;
         for(let item of value.data.items){
-            cpt++
+
+            let month : string
+
+            switch(item.timestamp.substring(4,6)){
+                case '01':
+                    month = 'Janvier'
+                    break
+                case '02':
+                    month = 'Fevrier'
+                    break
+                case '03':
+                    month = 'Mars'
+                    break
+                case '04':
+                    month = 'Avril'
+                    break
+                case '05':
+                    month = 'Mai'
+                    break
+                case '06':
+                    month = 'Juin'
+                    break
+                case '07':
+                    month = 'Juillet'
+                    break
+                case '08':
+                    month = 'Aout'
+                    break
+                case '09':
+                    month = 'Septembre'
+                    break
+                case '10':
+                    month = 'Octobre'
+                    break
+                case '11':
+                    month = 'Novembre'
+                    break
+                case '12':
+                    month = 'Decembre'
+                    break
+                default : 
+                month = ''
+                    break
+
+            }
+            
             monthly.push({
-                name : cpt + '-' + year,
+                name : month + ' ' + year,
                 value : item.views
             })
         }
